0784887a632ae4e7e7e52fa49b66f3309a1e4563
Thierry <thierryjefersson@gmail.com>
2024-12-10 15:07:53 -0300

    add `prisma`, `vite-tsconfig-paths`, file `.env` and `db.ts` for prisma client

--- src/tests/api/v1/status/get.test.ts
+++ src/tests/api/v1/status/get.test.ts
@@ -1,7 +1,14 @@
+import { db } from "@/infra/db";
 import { expect, test } from "vitest";
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+});
+
+test("Connect to database and should return some data", async () => {
+  const result = await db.task.findMany();
+
+  expect(result.length > 0).toBeTruthy();
 });
