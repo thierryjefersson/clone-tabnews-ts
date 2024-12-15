d374f79fff964cedb38dae84f8fa906550e5876f
Thierry <thierryjefersson@gmail.com>
2024-12-15 10:23:36 -0300

    finish endpoint `api/v1/status` status

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,5 +1,27 @@
+import { db } from "@/infra/database";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return NextResponse.json({ chave: "Primeiro endpoint" }, { status: 200 });
+export const GET = async () => {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await db.$queryRaw<
+    { server_version: string }[]
+  >`SHOW server_version;`;
+  const databaseMaxConnections = await db.$queryRaw<
+    { max_connections: string }[]
+  >`SHOW max_connections;`;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await db.$queryRaw<
+    { count: number }[]
+  >`SELECT count(*)::int FROM pg_stat_activity WHERE datname = ${databaseName};`;
+
+  return NextResponse.json({
+    updated_at: updatedAt,
+    database: {
+      version: databaseVersion[0].server_version,
+      max_connections: parseInt(databaseMaxConnections[0].max_connections),
+      opened_connections: databaseOpenedConnections[0].count,
+    },
+  });
 };
--- src/tests/api/v1/status/get.test.ts
+++ src/tests/api/v1/status/get.test.ts
@@ -1,25 +1,15 @@
-import { db } from "@/infra/db";
 import { expect, test } from "vitest";
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
-});
 
-test("Connect to database, create e return data", async () => {
-  const egData = {
-    id: 200,
-    name: "academia",
-    check: false,
-  };
-  const result = await db.task.create({ data: egData });
-  await db.task.delete({
-    where: {
-      id: egData.id,
-    },
-  });
+  const responseBody = await response.json();
 
-  console.log(result);
-  expect(result.name).toBe(egData.name);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.database.version).toEqual("16.0");
+  expect(responseBody.database.max_connections).toEqual(100);
+  expect(responseBody.database.opened_connections).toEqual(1);
 });
