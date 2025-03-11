449db733cc8ae8833a1b9d3131bccc2a3af12844
Thierry <thierryjefersson@gmail.com>
2025-03-11 14:20:03 -0300

    change `tailwindcss version` to v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1>
+    <h1 className="text-3xl font-bold underline">
       As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o
       efeito do banho, por isso recomenda-se diariamente. 😆
     </h1>
